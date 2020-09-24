42dec7d324af5496451914a013353306d47b296e
Yago Lopes <yagolopeslazaro@gmail.com>
2020-09-24 16:51:23 -0300

    Updates

---
+++ precache-manifest.61f64aa78464156403569a2fddb5fba6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e510c2bf904281ce9b04f2c340ce1c7f",
+    "url": "/youtube-clone-typeform-homepage/index.html"
+  },
+  {
+    "revision": "1e8f8e57fcd2fe1a1e7d",
+    "url": "/youtube-clone-typeform-homepage/static/js/2.c6aaa9cc.chunk.js"
+  },
+  {
+    "revision": "216e537bacc64f9dbaabd26649a9ab95",
+    "url": "/youtube-clone-typeform-homepage/static/js/2.c6aaa9cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ffe50f36c5f58e8f0af4",
+    "url": "/youtube-clone-typeform-homepage/static/js/main.13698e17.chunk.js"
+  },
+  {
+    "revision": "e83325fd9f6f300e0a13",
+    "url": "/youtube-clone-typeform-homepage/static/js/runtime-main.b63c5435.js"
+  }
+]);
